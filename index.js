20411d82c6bfc97e0122df139cdd20b41c58cb07
bessien <barbaraessien@gmail.com>
2017-04-24 15:37:17 -0700

    get rid of fluff

--- index.js
+++ index.js
@@ -1,45 +1,22 @@
 // This will import alexa-sdk and set up an Alexa object for us to work with.
-var Alexa = require('alexa-sdk');
-
-// exports.handler = function(event, context, callback){
-//     var alexa = Alexa.handler(event, context);
-// };
+var Alexa = require('alexa-sdk')
 
 // Handles the intents for our skill.
 var handlers = {
 
-// The handlers you define can call each other, making it possible to ensure your responses are uniform
-	'LaunchRequest': function () {
-        this.emit('HelloWorldIntent');
-    },
+    'SetSequence': function () {
+        const intent = this.event.request.intent
 
-    'HelloWorldIntent': function () {
-        this.emit(':tell', 'Hello World!');
-    }
+        const sequence = intent.slots.sequence.value
+
+        this.emit(':tell', `walking you through ${sequence}`);
+    },
 
 };
 
-// To ask the user for information we would instead use an :ask action. 
-//The difference between :ask and :tell is that after a :tell action, 
-//the session is ended without waiting for the user to provide more input.
-
-// this.emit(':ask', 'What would you like to do?', 'Please say that again?');
-
-// var speechOutput = 'Hello world!';
-// var repromptSpeech = 'Hello again!';
-
-// this.emit(':tell', speechOutput);
-
-// this.emit(':ask', speechOutput, repromptSpeech);
-
 // We need to register the event handlers using the registerHandlers function of the alexa object we just created.
- // exports.handler = function(event, context, callback) {
- //      var alexa = Alexa.handler(event, context);
- //      alexa.registerHandlers(handlers); //You can also register multiple handler objects at once alexa.registerHandlers(handlers, handlers2, handlers3, ...);
- // };
-
-// use the execute function from the alexa object to run your skill's logic.
-  exports.handler = function(event, context, callback) {
+// Use the execute function from the alexa object to run your skill's logic.
+exports.handler = (event, context, callback) => {
     var alexa = Alexa.handler(event, context);
     alexa.registerHandlers(handlers);
     alexa.execute();
